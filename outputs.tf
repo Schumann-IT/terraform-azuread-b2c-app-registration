4b66e7fa8bfa552955a2d6af36895516fb4f1e04
Jan Schumann <js@schumann-it.com>
2024-05-03 21:04:13 +0200

    fixes

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,20 @@
 output "display_name" {
-  value = azuread_application.this.display_name
+  description = "The display name of the application."
+  value       = azuread_application.this.display_name
 }
 
 output "object_id" {
-  value = azuread_application.this.object_id
+  description = "The object ID of the application."
+  value       = azuread_application.this.object_id
 }
 
 output "application_id" {
-  value = azuread_application.this.client_id
+  description = "The application (client) ID of the application."
+  value       = azuread_application.this.client_id
 }
 
 output "exposed_api_permissions" {
+  description = "The exposed API permissions of the application."
   value = {
     (azuread_application.this.client_id) = [
       for p in azuread_application_permission_scope.this : p.scope_id
@@ -19,5 +23,6 @@ output "exposed_api_permissions" {
 }
 
 output "admin_consent_required" {
-  value = length(var.config.required_resource_access) > 0
+  description = "Whether admin consent is required."
+  value       = length(var.config.required_resource_access) > 0
 }
