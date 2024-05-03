d2ab530cbe9d9c1005f900ced468960a6390357c
Jan Schumann <js@schumann-it.com>
2024-05-03 21:12:17 +0200

    fixes

--- examples/b2c-oidc-federation/main.tf
+++ examples/b2c-oidc-federation/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_version = ">= 1.4.0, < 2.0.0"
+
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = ">= 2.38.0"
+    }
+  }
+}
+
+provider "azuread" {}
+
 module "app" {
   source = "../../"
 
